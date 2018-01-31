152d8178633f486c4d76d359cb3377c6fbc63fb2
Joshua Carp <jm.carp@gmail.com>
2018-01-31 15:53:15 -0500

    Miscellaneous verification cleanup.

--- ci/verify-ds.sh
+++ ci/verify-ds.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-set -eu
-DNS_SERVERS=$(spruce json terraform-yaml/state.yml  | jq -r ".terraform_outputs.${ENVIRONMENT}_dns_public_ips[]")
+set -eux
+
+DNS_SERVERS=$(spruce json terraform-yaml/state.yml | jq -r ".terraform_outputs.${ENVIRONMENT}_dns_public_ips[]")
 
 for DNS_SERVER in ${DNS_SERVERS}; do
-  dig . DNSKEY @8.8.8.8| grep -Ev '^($|;)' > root.keys
+  dig . DNSKEY @8.8.8.8 | grep -Ev '^($|;)' > root.keys
 
   for ZONE in ${ZONES}; do
-    dig +dnssec +sigchase +trusted-keys=./root.keys ${ZONE}. A @${DNS_SERVER} | tail -n 2 | grep 'DNSSEC validation is ok: SUCCESS'
+    dig +dnssec +sigchase +trusted-key=./root.keys "${ZONE}." A "@${DNS_SERVER}" \
+      | grep 'DNSSEC validation is ok: SUCCESS'
   done
 done
