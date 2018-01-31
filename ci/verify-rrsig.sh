64df599d3820f33e22c3334886118a0b0fc1c94e
Steven Harms <steven.harms@gsa.gov>
2018-01-31 13:23:06 -0500

    Get DS from 8.8.8.8

--- ci/verify-rrsig.sh
+++ ci/verify-rrsig.sh
@@ -3,7 +3,7 @@ set -eu
 DNS_SERVERS=$(spruce json terraform-yaml/state.yml  | jq -r ".terraform_outputs.${ENVIRONMENT}_dns_public_ips[]")
 
 for DNS_SERVER in ${DNS_SERVERS}; do
-  dig . DNSKEY @${DNS_SERVER}| grep -Ev '^($|;)' > root.keys
+  dig . DNSKEY @8.8.8.8| grep -Ev '^($|;)' > root.keys
 
   for ZONE in ${ZONES}; do
     dig +sigchase +dnssec +trusted-key=./root.keys ${ZONE}. A @${DNS_SERVER} | grep -P '^;; VERIFYING A RRset for example.com. with DNSKEY:\d+: success$'
