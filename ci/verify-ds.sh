239b1a642f759d311f78fc8e5594e13a0f856d08
Steven Harms <steven.harms@gsa.gov>
2018-01-31 12:15:55 -0500

    Remove quotes

--- ci/verify-ds.sh
+++ ci/verify-ds.sh
@@ -2,10 +2,10 @@
 set -eu
 DNS_SERVERS=$(spruce json terraform-yaml/state.yml  | jq -r ".terraform_outputs.${ENVIRONMENT}_dns_public_ips[]")
 
-for DNS_SERVER in "${DNS_SERVERS}"; do
+for DNS_SERVER in ${DNS_SERVERS}; do
   dig . DNSKEY @${DNS_SERVER}| grep -Ev '^($|;)' > root.keys
 
-  for ZONE in "${ZONES}"; do
+  for ZONE in ${ZONES}; do
     dig +dnssec +sigchase +trusted-keys=./root.keys ${ZONE}. A @${DNS_SERVER} | tail -n 2 | grep 'DNSSEC validation is ok: SUCCESS'
   done
 done
