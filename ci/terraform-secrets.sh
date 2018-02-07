f4ecf0ee9ac53bf93ac26749bb4cd43967103019
Steven Harms <steven.harms@gsa.gov>
2018-02-07 11:48:57 -0500

    Update to populate ns servers dynamically

--- ci/terraform-secrets.sh
+++ ci/terraform-secrets.sh
@@ -6,3 +6,11 @@ spruce merge --prune terraform_outputs \
   pdns-config/varsfiles/terraform.yml \
   terraform-yaml/state.yml \
   > terraform-secrets/terraform.yml
+
+count=0
+echo "nameservers: |" > terraform-secrets/ns.yml
+for nameserver in $(bosh interpolate terraform-secrets/terraform.yml --path /master-ips | tr ";" "\n")
+do
+  count=$((count+1))
+  echo "    ns${count} IN A ${nameserver}" >> terraform-secrets/ns.yml
+done
