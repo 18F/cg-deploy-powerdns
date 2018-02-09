81c1ec6b96c698b97e62d0566d62e3a8a62a0135
Steven Harms <steven.harms@gsa.gov>
2018-02-09 11:20:13 -0500

    Only get root keys once

--- ci/verify-rrsig.sh
+++ ci/verify-rrsig.sh
@@ -3,9 +3,8 @@
 set -eux
 DNS_SERVERS=$(spruce json terraform-yaml/state.yml | jq -r ".terraform_outputs.${ENVIRONMENT}_dns_public_ips[]")
 
+dig . DNSKEY @8.8.8.8 | grep -Ev '^($|;)' > root.keys
 for DNS_SERVER in ${DNS_SERVERS}; do
-  dig . DNSKEY @8.8.8.8 | grep -Ev '^($|;)' > root.keys
-
   for ZONE in ${ZONES}; do
     dig +sigchase +dnssec +trusted-key=./root.keys "${ZONE}." A "@${DNS_SERVER}" \
       | grep -P "^;; VERIFYING A RRset for ${ZONE}. with DNSKEY:\d+: success$"
