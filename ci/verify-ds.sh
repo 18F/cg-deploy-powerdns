ea7fc8f98f25f43cb07b912ee8f10d9461063733
Steven Harms <steven.harms@gsa.gov>
2018-02-12 11:22:54 -0500

    Loop over zones

--- ci/verify-ds.sh
+++ ci/verify-ds.sh
@@ -4,4 +4,6 @@ set -eux
 DNS_SERVERS=$(spruce json terraform-yaml/state.yml | jq -r ".terraform_outputs.${ENVIRONMENT}_dns_public_ips[]")
 
 dig . DNSKEY @8.8.8.8 | grep -Ev '^($|;)' > root.keys
-dig +dnssec +sigchase +trusted-key=./root.keys "${ZONE}." A @8.8.8.8 | grep 'DNSSEC validation is ok: SUCCESS'
+for ZONE in ${ZONES}; do
+  dig +dnssec +sigchase +trusted-key=./root.keys "${ZONE}." A @8.8.8.8 | grep 'DNSSEC validation is ok: SUCCESS'
+done
