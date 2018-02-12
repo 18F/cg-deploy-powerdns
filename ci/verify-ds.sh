7304fa2117eeceedf9471c47adc3136bdb8b146c
Steven Harms <steven.harms@gsa.gov>
2018-02-12 11:08:40 -0500

    Need to check google instead, ours don't recurse

--- ci/verify-ds.sh
+++ ci/verify-ds.sh
@@ -4,9 +4,4 @@ set -eux
 DNS_SERVERS=$(spruce json terraform-yaml/state.yml | jq -r ".terraform_outputs.${ENVIRONMENT}_dns_public_ips[]")
 
 dig . DNSKEY @8.8.8.8 | grep -Ev '^($|;)' > root.keys
-for DNS_SERVER in ${DNS_SERVERS}; do
-  for ZONE in ${ZONES}; do
-    dig +dnssec +sigchase +trusted-key=./root.keys "${ZONE}." A "@${DNS_SERVER}" \
-      | grep 'DNSSEC validation is ok: SUCCESS'
-  done
-done
+dig +dnssec +sigchase +trusted-key=./root.keys "${ZONE}." A @8.8.8.8 | grep 'DNSSEC validation is ok: SUCCESS'
